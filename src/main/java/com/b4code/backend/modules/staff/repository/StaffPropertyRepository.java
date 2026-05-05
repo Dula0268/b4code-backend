2205a60198d7cb8eb8dfa75cd4ca36f0d79a3d10
Staff User <staff@primestay.com>
2026-05-05 11:26:29 +0530

    Add staff property repository with query methods

--- src/main/java/com/b4code/backend/modules/staff/repository/StaffPropertyRepository.java
+++ src/main/java/com/b4code/backend/modules/staff/repository/StaffPropertyRepository.java
@@ -2,15 +2,18 @@ package com.b4code.backend.modules.staff.repository;
 
 import com.b4code.backend.modules.staff.entity.StaffProperty;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public interface StaffPropertyRepository extends JpaRepository<StaffProperty, Long> {
-
-    Optional<StaffProperty> findByStaffIdAndPropertyId(Long staffId, Long propertyId);
-
     List<StaffProperty> findByStaffId(Long staffId);
-
+    List<StaffProperty> findByPropertyId(Long propertyId);
+    Optional<StaffProperty> findByStaffIdAndPropertyId(Long staffId, Long propertyId);
+    
+    @Transactional
     void deleteByStaffIdAndPropertyId(Long staffId, Long propertyId);
 }
