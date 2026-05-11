d26c26e24b25a2986e70f3bbbf18bd19ceb0ea48
Dulanga <shehandulanga112@gmail.com>
2026-05-11 15:31:10 +0530

    feat: add development configuration and order-related JPA repositories

---
+++ src/main/java/com/b4code/backend/modules/staff/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.b4code.backend.modules.staff.repository;
+
+import com.b4code.backend.modules.staff.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
